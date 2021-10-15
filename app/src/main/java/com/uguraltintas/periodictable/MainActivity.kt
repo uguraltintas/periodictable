6c26c581781d16dd3dc2f5a992c4d79d8370878a
Uğur Altıntaş <uguraltintas17@gmail.com>
2021-10-15 22:32:55 +0300

    add splash screen

--- app/src/main/java/com/uguraltintas/periodictable/MainActivity.kt
+++ app/src/main/java/com/uguraltintas/periodictable/MainActivity.kt
@@ -1,41 +1,37 @@
 package com.uguraltintas.periodictable
 
-import android.app.AlertDialog
 import android.app.Dialog
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
 import android.view.LayoutInflater
-import android.widget.ImageView
-import android.widget.Toast
+import android.view.Window
 import androidx.databinding.DataBindingUtil
 import androidx.recyclerview.widget.GridLayoutManager
-import androidx.recyclerview.widget.LinearLayoutManager
-import androidx.recyclerview.widget.RecyclerView
-import com.bumptech.glide.Glide
 import com.uguraltintas.periodictable.databinding.ActivityMainBinding
 import com.uguraltintas.periodictable.databinding.DialogElementBinding
 
-class MainActivity : AppCompatActivity(),ElementsAdapter.OnItemClickListener {
+class MainActivity : AppCompatActivity(), ElementsAdapter.OnItemClickListener {
     private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
-
+        overridePendingTransition(R.anim.fade_in, R.anim.fade_out)
         val list = Elements.getElementData()
-        binding.rvElements.adapter = ElementsAdapter(list,this)
+        binding.rvElements.adapter = ElementsAdapter(list, this)
         binding.rvElements.layoutManager = GridLayoutManager(this, 18)
     }
 
     override fun onClick(element: Element) {
-        if(element.symbol != "") {
+        if (element.symbol != "") {
             showDialog(element)
         }
     }
-    private fun showDialog(element: Element){
+
+    private fun showDialog(element: Element) {
         val dialog = Dialog(this)
         val inflater = LayoutInflater.from(this)
         val dialogBinding = DialogElementBinding.inflate(inflater)
+        dialog.requestWindowFeature(Window.FEATURE_NO_TITLE);
         dialog.setContentView(dialogBinding.root)
         dialogBinding.element = element
         dialog.show()
